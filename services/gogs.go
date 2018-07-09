4cd0bded1fe11eaa36869444c644c5d746ac8a7e
codestation <codestation404@gmail.com>
2018-07-09 12:23:05 -0500

    do not run gogs app using gosu wrapper

--- services/gogs.go
+++ services/gogs.go
@@ -18,9 +18,12 @@ package services
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
+	"syscall"
 	"time"
 )
 
@@ -30,12 +33,26 @@ type Gogs struct {
 
 var GogsAppPath = "/app/gogs/gogs"
 
+func getEnvInt(key string, def int) int {
+	value := os.Getenv(key)
+
+	if value != "" {
+		val, err := strconv.Atoi(value)
+		if err == nil {
+			return val
+		} else {
+			log.Printf("cannot parse env key %s with value %s", key, value)
+		}
+	}
+
+	return def
+}
+
 func (g *Gogs) Backup() (string, error) {
 	filepath := fmt.Sprintf("gogs-backup-%s.zip", time.Now().Format("20060102150405"))
 
 	args := []string{
-		"git",
-		GogsAppPath, "backup",
+		"backup",
 		"--target", SaveDir,
 		"--archive-name", filepath,
 	}
@@ -44,9 +61,21 @@ func (g *Gogs) Backup() (string, error) {
 		args = append(args, "--config", g.ConfigPath)
 	}
 
-	cmd := exec.Command("gosu", args...)
+	cmd := exec.Command(GogsAppPath, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	// only switch user when running as root
+	if os.Geteuid() == 0 {
+		// run process as git user
+		uid := uint32(getEnvInt("PUID", 1000))
+		gid := uint32(getEnvInt("PGID", 1000))
+
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uid, Gid: gid}
+	} else {
+		log.Printf("not runnign as root, starting %s with UID %d", GogsAppPath, os.Geteuid())
+	}
 
 	env := os.Environ()
 	cmd.Env = append(env, "USER=git")
@@ -60,8 +89,7 @@ func (g *Gogs) Backup() (string, error) {
 
 func (g *Gogs) Restore(filepath string) error {
 	args := []string{
-		"git",
-		GogsAppPath, "restore",
+		"restore",
 		"--from", filepath,
 	}
 
@@ -69,9 +97,21 @@ func (g *Gogs) Restore(filepath string) error {
 		args = append(args, "--config", g.ConfigPath)
 	}
 
-	cmd := exec.Command("gosu", args...)
+	cmd := exec.Command(GogsAppPath, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	// only switch user when running as root
+	if os.Geteuid() == 0 {
+		// run process as git user
+		uid := uint32(getEnvInt("PUID", 1000))
+		gid := uint32(getEnvInt("PGID", 1000))
+
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uid, Gid: gid}
+	} else {
+		log.Printf("not runnign as root, starting %s with UID %d", GogsAppPath, os.Geteuid())
+	}
 
 	env := os.Environ()
 	cmd.Env = append(env, "USER=git")
