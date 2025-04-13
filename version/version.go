b189bacf51e38c19ae15f2d5fdd7762dc82904c7
codestation <codestation@megpoid.dev>
2025-04-13 16:20:33 -0500

    update licenses

--- version/version.go
+++ version/version.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 codestation
+Copyright 2025 codestation
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
